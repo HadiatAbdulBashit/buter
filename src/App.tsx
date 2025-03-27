a0b791663f9e0ba45c6d670d839e0fc38bc4c829
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2025-03-27 14:31:24 +0700

    feat: add theme provider and install components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,82 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Button } from "./components/ui/button";
+import { ScrollArea, ScrollBar } from "./components/ui/scroll-area";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+
+  const [year, setYear] = useState<number>(currentYear);
+  const [activeMonth, setActiveMonth] = useState<number>(currentMonth);
+
+  const months = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
+  const filteredMonths = year === currentYear ? months.slice(0, currentMonth + 1) : months;
+
+  const handleYearChange = (value: string) => {
+    const newYear = Number(value);
+    setYear(newYear);
+    if (newYear === currentYear) {
+      setActiveMonth(currentMonth);
+      return;
+    }
+    setActiveMonth(filteredMonths.length - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='container mx-auto py-4'>
+        <h1>Monager</h1>
+        <div className='flex gap-2 items-center mb-8'>
+          <ScrollArea className='whitespace-nowrap overflow-hidden'>
+            <div className='flex w-max space-x-2'>
+              {filteredMonths.map((month, index) => (
+                <Button
+                  className={`capitalize shrink-0`}
+                  variant={index === activeMonth ? "default" : "secondary"}
+                  key={index}
+                  onClick={() => setActiveMonth(index)}
+                >
+                  {month}
+                </Button>
+              ))}
+            </div>
+            <ScrollBar orientation='horizontal' />
+          </ScrollArea>
+
+          <Select onValueChange={handleYearChange} defaultValue={year.toString()}>
+            <SelectTrigger className='w-fit'>
+              <SelectValue placeholder='Pilih Tahun' />
+            </SelectTrigger>
+            <SelectContent>
+              {[...Array(5)].map((_, i) => (
+                <SelectItem key={currentYear - i} value={(currentYear - i).toString()}>
+                  {currentYear - i}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className='grid grid-cols-3 gap-4'>
+          <div>grapf</div>
+          <div>grapf</div>
+          <div></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      <Button>test</Button>
     </>
   );
 }
